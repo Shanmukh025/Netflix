ba3ed336b8ea575a4662c88f9f45ffa5de609d86
Shanmukh025 <shanmukh2564@gmail.com>
2023-12-19 10:52:27 +0530

    Made Responsive

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -3,7 +3,6 @@ import dim from '../assets/3d.png'
 import doc from '../assets/documentary.png'
 import smartphone from '../assets/smartphone.png'
 import tv from '../assets/tv.png'
-import PlayCard from './PlayCard'
 
 const Info = () => {
     const CollapsibleSection = ({ title, content }) => {
@@ -108,9 +107,35 @@ const Info = () => {
                     </div>
                 </article>
             </section>
-
-            <section className="info-box-4"></section>
-
+            <section className="info-box-4">
+            <h3 className="heading">Frequently Asked Questions</h3>
+            <div className='collapsible'>
+        <CollapsibleSection
+            title="What is Netflix?"
+            content={<p>Netflix is a streaming service that offers a wide variety of award-winning TV shows, movies, anime, documentaries and more – on thousands of internet-connected devices.<br/>
+                You can watch as much as you want, whenever you want, without a single ad – all for one low monthly price. There's always something new to discover, and new TV shows and movies are added every week!
+                </p>}
+        />
+        <CollapsibleSection
+            title="Where can I watch?"
+            content={<p>Watch anywhere, anytime. Sign in with your Netflix account to watch instantly on the web at netflix.com from your personal computer or on any internet-connected device that offers the Netflix app, including smart TVs, smartphones, tablets, streaming media players and game consoles.<br/>
+            </p>}
+        />
+        <CollapsibleSection
+            title="What can I watch on Netflix?"
+            content={<p>Netflix has an extensive library of feature films, documentaries, TV shows, anime, award-winning Netflix originals, and more. Watch as much as you want, anytime you want.</p>}
+        />
+        <CollapsibleSection
+            title="How do I cancel Netflix?"
+            content={<p>Netflix is flexible. There are no annoying contracts and no commitments. You can easily cancel your account online in two clicks. There are no cancellation fees – start or stop your account anytime.</p>}
+        />
+        <CollapsibleSection
+            title="Is Netflix safe for Kids?"
+            content={<p>The Netflix Kids experience is included in your membership to give parents control while kids enjoy family-friendly TV shows and films in their own space.<br/>
+                Kids profiles come with PIN-protected parental controls that let you restrict the maturity rating of content kids can watch and block specific titles you don’t want kids to see.</p>}
+        />
+        </div>
+            </section>
         </div>
     </>
 )
