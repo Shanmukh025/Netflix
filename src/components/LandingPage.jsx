47514b750184ffa4699c3e95c56631515d3ad6f9
Shanmukh025 <shanmukh2564@gmail.com>
2023-12-21 22:42:55 +0530

    improved loading

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -5,22 +5,24 @@ import Loading from './Loading';
 import Navbar from './Navbar';
 import Signup from './Signup';
 
-
 const LandingPage = () => {
     const [isLoading, setIsLoading] = useState(true);
-
     useEffect(() => {
-        const timer = setTimeout(() => {
+        const hasVisited = localStorage.getItem('visited');
+        if (hasVisited) {
             setIsLoading(false);
-        }, 3000);
-
-        return () => clearTimeout(timer);
+        } else {
+            const timer = setTimeout(() => {
+                setIsLoading(false);
+                
+                localStorage.setItem('visited', 'true');
+            }, 3000);
+            return () => clearTimeout(timer);
+        }
     }, []);
-
     if (isLoading) {
         return <Loading />;
-}
-
+    }
     return (
     <>
     <Navbar/>
